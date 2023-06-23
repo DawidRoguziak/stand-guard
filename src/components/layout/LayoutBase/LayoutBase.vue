cbddb3e4d813d92c2fdd3038034351293582b775
dawid <dawidroguziak@gmail.com>
2023-06-23 21:06:24 +0200

    change logo to img

--- src/components/layout/LayoutBase/LayoutBase.vue
+++ src/components/layout/LayoutBase/LayoutBase.vue
@@ -4,7 +4,8 @@
       <el-header class="dark:bg-slate-900/60 bg-slate-100 md:hidden app-header">
         <div class="flex justify-between place-items-center h-full">
           <div class="text-gray-50">
-            <h3 class="dark:text-gray-50 text-teal-700 mobile-logo-layout">
+            <h3 class="dark:text-gray-50 text-teal-700 ">
+              <SvgLogo style="width: 100px;height: 40px;"/>
             </h3>
           </div>
           <span class="material-symbols-outlined cursor-pointer dark:text-gray-50 text-teal-700" @click="toggleAside">
@@ -21,8 +22,8 @@
            max-sm:absolute max-sm:top-0 max-sm:left-0 max-sm:bottom-0 max-sm:z-50
            max-sm:overflow-auto
           ">
-          <div class="mb-5 mt-5 max-md:hidden visible">
-            <SvgLogo style="width: 100%;height: 50px;"/>
+          <div class="mb-5 mt-5  ">
+            <SvgLogo style="width: 100px;height: 50px;" />
           </div>
           <UiMenu/>
           <!--          delete this component after work-->
