cf9f8eca2a7f6ea61919592491b52fba5bbf7291
dawid <dawidroguziak@gmail.com>
2023-07-29 23:42:54 +0200

    create plan form

--- src/components/ui/form-elements/UiSelect/UiSelect.vue
+++ src/components/ui/form-elements/UiSelect/UiSelect.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
 import type {UiSelectProps} from "@/components/ui/form-elements/UiSelect/UiSelectProps";
-import {onUnmounted, ref} from "vue";
+import {ref} from "vue";
 import {ErrorMessage, Field} from "vee-validate";
 
 const {placeholder, name, options, teleported = true} = defineProps<UiSelectProps>();
 const internalValue = ref<any>()
-
-onUnmounted(() => {
-  console.log('unmounted')
-
-})
 
 </script>
 
